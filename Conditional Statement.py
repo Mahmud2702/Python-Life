95783e7e18d286cc69b47dbb9f4b8734b7e9cbc7
Mahmud2702 <161118258+Mahmud2702@users.noreply.github.com>
2024-09-23 12:43:40 +0600

    Add files via upload

---
+++ Conditional Statement.py
@@ -0,0 +1,10 @@
+number = int(input("Enter the marks: "))
+
+if(number>=90):
+    print("A")
+elif(number>=80 and number<90):
+    print("B")
+elif(number>=70 and number<80):
+    print("C")
+else:
+    print("D")
