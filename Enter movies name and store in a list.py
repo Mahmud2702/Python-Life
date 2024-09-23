b978e87a1272db2adfa8d8c7241dadc2f69c0785
Mahmud2702 <161118258+Mahmud2702@users.noreply.github.com>
2024-09-23 13:05:38 +0600

    Add files via upload

---
+++ Enter movies name and store in a list.py
@@ -0,0 +1,18 @@
+a = str(input("Enter 1st name: "))
+b = str(input("Enter 2nd name: "))
+c = str(input("Enter 3rd name: "))
+
+list=[a,b,c]
+print(list)
+
+#Another_process
+
+movies =[]
+movie1 = input("Enter 1st name: ")
+movie2 = (input("Enter 2nd name: "))
+movie3 =(input("Enter 3rd name: "))
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print(movies)
