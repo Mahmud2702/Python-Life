8764cc8bc81370dbe3e066573347e198b64a8124
Mahmud2702 <161118258+Mahmud2702@users.noreply.github.com>
2024-09-23 12:07:50 +0600

    Add files via upload

---
+++ String Functions.py
@@ -0,0 +1,20 @@
+#endswith
+str = "I am a boy"
+print(str.endswith("I"))
+print(str.endswith("oy"))
+
+#Capitalize
+str1 = "i am a boy"
+print(str1.capitalize())
+
+#Replace
+str2 = "I am a boy"
+print(str.replace("boy","girl"))
+
+#Find
+str3 = "I am a boy"
+print(str3.find("o")) 
+
+#
+str5 = "I am a boy"
+print(str5.count("a"))
